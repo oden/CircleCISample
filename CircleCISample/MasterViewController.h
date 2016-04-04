0f21bfd88532fab9b98db596d4fa876a4916a51d
oden <oden@jitakukeibi.in>
2016-04-04 22:45:35 +0900

    Initial Commit

---
+++ CircleCISample/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CircleCISample
+//
+//  Created by 小田 雄帆 on 2016/04/04.
+//  Copyright © 2016年 JITAKUKEIBI INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
